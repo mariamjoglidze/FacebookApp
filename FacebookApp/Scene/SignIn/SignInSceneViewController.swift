979d6ee77b17978e6caf8c32d2a36118a91d7ee1
Mariam Joglidze <marijoglidze@gmail.com>
2022-01-17 18:21:17 +0400

    SignIn with Facebook
    SignIn with Facebook

--- FacebookApp/Scene/SignIn/SignInSceneViewController.swift
+++ FacebookApp/Scene/SignIn/SignInSceneViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class SignInSceneViewController: UIViewController {
 
+class SignInSceneViewController: UIViewController, LoginButtonDelegate {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let token = AccessToken.current,
+           !token.isExpired {
+            let token = token.tokenString
+            let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                     parameters: ["fields": "email, name"],
+                                                     tokenString: token,
+                                                     version: nil,
+                                                     httpMethod: .get)
+            
+            request.start(completion:{ connection, result, error in
+                print("\(result)")
+            })
+        } else {
+            let loginButton = FBLoginButton()
+            loginButton.center = view.center
+            loginButton.delegate = self
+            loginButton.permissions = ["public_profile", "email"]
+            
+            view.addSubview(loginButton)
+        }
+        
+        
+        
     }
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        let token = result?.token?.tokenString
+        let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                 parameters: ["fields": "email, name"],
+                                                 tokenString: token,
+                                                 version: nil,
+                                                 httpMethod: .get)
+        
+        request.start(completion:{ connection, result, error in
+            print("\(result)")
+        })
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
 
-
+    }
+    
+    
 }
 

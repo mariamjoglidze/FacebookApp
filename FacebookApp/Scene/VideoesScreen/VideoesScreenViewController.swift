0c319da71c111213ef39650395e6db0162bd7f13
Mariam Joglidze <marijoglidze@gmail.com>
2022-01-20 19:51:49 +0400

    VideoScreen
    Add VideoScreen

--- FacebookApp/Scene/VideoesScreen/VideoesScreenViewController.swift
+++ FacebookApp/Scene/VideoesScreen/VideoesScreenViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class VideoesScreenViewController: BaseViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
+        tableView.registerNib(class: VideoCell.self)
+        
     }
     
 }
+
+
+extension VideoesScreenViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10   }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.deque(class: VideoCell.self, for: indexPath)
+        
+        return cell
+    }
+}
+

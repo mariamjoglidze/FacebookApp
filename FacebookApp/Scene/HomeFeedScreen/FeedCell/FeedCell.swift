6dd525dfb0867d9be8394b1767fd3e13708c816a
Mariam Joglidze <marijoglidze@gmail.com>
2022-01-21 10:38:46 +0400

    FeedCell
    create Home Feed table view Cell

--- FacebookApp/Scene/HomeFeedScreen/FeedCell/FeedCell.swift
+++ FacebookApp/Scene/HomeFeedScreen/FeedCell/FeedCell.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class FeedCell: UITableViewCell {
 
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var postDescriptionLabel: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        userImage.layer.cornerRadius = userImage.bounds.width / 2
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- FacebookApp/Scene/HomeFeedScreen/HomeFeedScreenViewController.swift
+++ FacebookApp/Scene/HomeFeedScreen/HomeFeedScreenViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class HomeFeedScreenViewController: BaseViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
+        tableView.registerNib(class: FeedCell.self)
+        
     }
     
-
 }
+
+
+extension HomeFeedScreenViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10   }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.deque(class: FeedCell.self, for: indexPath)
+        
+        return cell
+    }
+}
+
